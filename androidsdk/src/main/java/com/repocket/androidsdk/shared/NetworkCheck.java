94699451c2753248a88a80d47f76fa645794a8af
Nabeel Hassan <nabeelbaghoor@gmail.com>
2023-10-17 00:57:50 +0500

    Update deprecated stuff

--- androidsdk/src/main/java/com/repocket/androidsdk/shared/NetworkCheck.java
+++ androidsdk/src/main/java/com/repocket/androidsdk/shared/NetworkCheck.java
@@ -3,7 +3,8 @@ package com.repocket.androidsdk.shared;
 import android.app.Activity;
 import android.content.Context;
 import android.net.ConnectivityManager;
-import android.net.NetworkInfo.State;
+import android.net.Network;
+import android.net.NetworkCapabilities;
 
 public class NetworkCheck {
     private Context context;
@@ -11,42 +12,98 @@ public class NetworkCheck {
     private Activity activity;
     private ConnectivityManager connectivityManager;
 
-    public NetworkCheck() {
-
-        instance = this;
+    private NetworkCheck() {
     }
 
     public static NetworkCheck instance() {
         if (instance == null) {
             instance = new NetworkCheck();
         }
-
         return instance;
     }
 
     public void setContext(Context context) {
         this.context = context;
-        this.activity = (Activity)context;
+        this.activity = (Activity) context;
     }
 
     public boolean CheckWifi() {
-        if (this.connectivityManager == null) {
-            this.connectivityManager = (ConnectivityManager)this.activity.getSystemService(Context.CONNECTIVITY_SERVICE);
+        if (connectivityManager == null) {
+            connectivityManager = (ConnectivityManager) context.getSystemService(Context.CONNECTIVITY_SERVICE);
         }
-
-        return this.connectivityManager.getNetworkInfo(1).getState() == State.CONNECTED;
+        Network network = connectivityManager.getActiveNetwork();
+        if (network != null) {
+            NetworkCapabilities capabilities = connectivityManager.getNetworkCapabilities(network);
+            return capabilities.hasTransport(NetworkCapabilities.TRANSPORT_WIFI);
+        }
+        return false;
     }
 
     public boolean CheckMobile() {
-        if (this.connectivityManager == null) {
-            this.connectivityManager = (ConnectivityManager)this.activity.getSystemService(Context.CONNECTIVITY_SERVICE);
+        if (connectivityManager == null) {
+            connectivityManager = (ConnectivityManager) context.getSystemService(Context.CONNECTIVITY_SERVICE);
         }
-
-        return this.connectivityManager.getNetworkInfo(0).getState() == State.CONNECTED;
+        Network network = connectivityManager.getActiveNetwork();
+        if (network != null) {
+            NetworkCapabilities capabilities = connectivityManager.getNetworkCapabilities(network);
+            return capabilities.hasTransport(NetworkCapabilities.TRANSPORT_CELLULAR);
+        }
+        return false;
     }
 
     public boolean CheckInternet() {
-        return this.CheckMobile() || this.CheckWifi();
+        return CheckMobile() || CheckWifi();
     }
 }
 
+//
+//import android.app.Activity;
+//import android.content.Context;
+//import android.net.ConnectivityManager;
+//import android.net.NetworkInfo.State;
+//
+//public class NetworkCheck {
+//    private Context context;
+//    private static NetworkCheck instance;
+//    private Activity activity;
+//    private ConnectivityManager connectivityManager;
+//
+//    public NetworkCheck() {
+//
+//        instance = this;
+//    }
+//
+//    public static NetworkCheck instance() {
+//        if (instance == null) {
+//            instance = new NetworkCheck();
+//        }
+//
+//        return instance;
+//    }
+//
+//    public void setContext(Context context) {
+//        this.context = context;
+//        this.activity = (Activity)context;
+//    }
+//
+//    public boolean CheckWifi() {
+//        if (this.connectivityManager == null) {
+//            this.connectivityManager = (ConnectivityManager)this.activity.getSystemService(Context.CONNECTIVITY_SERVICE);
+//        }
+//
+//        return this.connectivityManager.getNetworkInfo(1).getState() == State.CONNECTED;
+//    }
+//
+//    public boolean CheckMobile() {
+//        if (this.connectivityManager == null) {
+//            this.connectivityManager = (ConnectivityManager)this.activity.getSystemService(Context.CONNECTIVITY_SERVICE);
+//        }
+//
+//        return this.connectivityManager.getNetworkInfo(0).getState() == State.CONNECTED;
+//    }
+//
+//    public boolean CheckInternet() {
+//        return this.CheckMobile() || this.CheckWifi();
+//    }
+//}
+//
